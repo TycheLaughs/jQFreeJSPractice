1500588b027cc6ff85778fb79648d4c716132a43
TycheLaughs <sksouza.art@gmail.com>
2015-06-18 13:49:52 -0400

    more adding, removing, modifying elements

--- jqfScript.js
+++ jqfScript.js
@@ -7,3 +7,26 @@ paras[2].innerHTML = "[Insert pithy phrase here -jQFree]";
 /*selected a group of elements and replaced one of them without
 really needing to use the array slice trick.  May need that later, 
 but not necessary to explore differences between jQuery and vanilla JS.*/
+
+/* module 3: Native DOM */
+var hUndG = document.createElement("p");
+/*way to add an element*/
+hUndG.innerText = "And now for something completely different -jQFree: Maggie startled badly enough that the mechanism controlling her chopping speed went into a frantic spin at her elbow joint, knife fairly vibrating in place, cutting nothing but air; she would have dropped it had she been holding it in her hand. She did drop the tuber. ";
+
+hUndG.id = "hug";
+paras[3].appendChild(hUndG);
+
+/*style the element I just added*/
+var hug = document.getElementById('hug');
+hug.style["color"]="magenta";
+hug.style["opacity"]=".8";
+
+/*remove elements*/
+var removalTwo = document.getElementById("boggart");
+removalTwo.parentNode.removeChild(removalTwo);
+
+/*probably going to add forms and dealing with those elements when going through the AJAX module, module 5*/
+
+
+
+/* module 4: Native Events */
--- script.js
+++ script.js
@@ -4,3 +4,23 @@
 var $pars = $("p");
 $pars.eq(1).append('<br>[just some edits -jQ]');
 //selected a group of elements and appended text to one of them
+
+/* module 3: Native DOM */
+var $bodily = $("#snippet");
+/*another way to add an element*/
+$bodily.prepend('<h3 id="subheader">Assorted chunks of my stock writing samples that are awaiting revision -jQ</h3>');
+var $sub = $("#subheader");
+
+/*style the element I just added*/
+$sub.css("color", "gray");
+$sub.css("font-size","1.2em");
+
+/*remove elements*/
+var removalOne = $(".ridikkulus");
+removalOne.empty();
+
+/*probably going to add forms and dealing with those elements when going through the AJAX module, module 5*/
+
+
+
+/* module 4: Native Events */
